3c6392412354fca43d52a0681b6629a43d1f4171
David Nolen <david.nolen@gmail.com>
2009-07-04 09:19:47 -0400

    * server.py: removing server.py, shiftserver.py now works.

--- shiftserver.py
+++ shiftserver.py
@@ -1,17 +1,72 @@
 import time
-
 import cherrypy
+
+import routes
+import shift
+import groups
 import simplejson as json
 
-class HelloWorld(object):
-    @cherrypy.expose
-    def index(self):
-        yield "Wow! The current time is %s<br />" % time.ctime()
-        yield "hmm %s" % json.dumps({"foo":"bar"})
-        yield "A <a href='static/test.txt'>static file</a> served by Apache."
+class User:
+    exposed = True
 
-appconf = {'/': {'tools.proxy.on':True,}
-       }
+    def POST(self, data):
+        return "Trying to create a user"
+
+    def GET(self, userName):
+        return "User name is %s" % userName
+
+    def PUT(self, data):
+        return "Updating a user"
+
+    def DELETE(self, userName):
+        return "Trying to delete %s" % userName
+
+
+class Shift:
+    exposed = True
+
+    def POST(self, data):
+        return "Trying to create a shift"
+
+    def GET(self, id):
+        return "Getting shift %s" % id
+
+    def PUT(self, data):
+        return "Updating a shift"
+
+    def DELETE(self, id):
+        return "Trying to delete a shift"
+
+
+class Shifts:
+    exposed = True
+
+    def GET(self, userName):
+        return json.dumps(shift.byUserName(userName))
+
+
+class Groups:
+    exposed = True
+
+    def GET(self, id):
+        return json.dumps(groups.inGroup(int(id)))
+
+
+class ShiftSpaceServer:
+    exposed = True
+    shift = Shift()
+    shifts = Shifts()
+    group = Groups()
+    user = User()
+
+    def GET(self):
+        return "ShiftSpace Server 1.0"
+
+
+appconf = {'/': {'tools.proxy.on':True,
+                 'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
 
 cherrypy.config.update({'server.socket_port':8080})
-cherrypy.quickstart(HelloWorld(), '/~davidnolen/shiftspace/shiftserver', appconf)
+
+# TODO: The following value should be read from an environment file - David 7/4/09
+cherrypy.quickstart(ShiftSpaceServer(), '/~davidnolen/shiftspace/shiftserver', appconf)
