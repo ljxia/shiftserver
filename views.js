1de6540875e6b64fda776259717ae2b97a81fdd8
David Nolen <david.nolen@gmail.com>
2009-07-06 12:40:22 -0400

    * views.js: added byhref and bydomain views.

--- views.js
+++ views.js
@@ -43,3 +43,24 @@ function(doc)
     emit(doc.createdBy, doc);
   }
 }
+
+// byhref
+// -----------------------------------------------------------------------------
+function(doc) {
+  if(doc.type == "shift")
+  {
+    emit(doc.href, doc);
+  }
+}
+
+// bydomain
+// -----------------------------------------------------------------------------
+function (doc) {
+  if(doc.type == "shift")
+  {
+    var href = doc.href;
+    var url = href.substr(7, href.length);
+    var parts = url.split("/");
+    emit("http://"+parts[0], doc);
+  }
+}
