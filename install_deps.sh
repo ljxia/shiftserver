5a749175766f246990fe9ac253141c5367e58822
David Nolen <david.nolen@gmail.com>
2009-09-15 22:27:58 -0400

    * install_deps.sh: fixed typo preventing install of httplib2.

--- install_deps.sh
+++ install_deps.sh
@@ -2,7 +2,7 @@ cd deps/CherryPy-3.1.2
 sudo python setup.py install
 cd ../Routes-1.10.3
 sudo python setup.py install
-cd ../httplib-0.4.0
+cd ../httplib2-0.4.0
 sudo python setup.py install
 cd ../simplejson-2.0.9
 sudo python setup.py install
